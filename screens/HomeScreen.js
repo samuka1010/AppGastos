d984ef76fcac6bbdf4b6947a3fbfab76cf2907a9
Samuel Franco <samukafranco@gmail.com>
2019-10-16 21:47:19 -0300

    Add header and footer

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,41 +10,57 @@ import {
   View,
   Dimensions
 } from 'react-native';
-import {Card, Chip, Avatar} from 'react-native-paper';
-import { MonoText } from '../components/StyledText';
-import * as Progress from 'react-native-progress';
+import {Card, Avatar} from 'react-native-paper';
+import ProgressBarAnimated from 'react-native-progress-bar-animated';
 
 // <Progress.Bar progress={0.3} width={200} color='blue' />
 export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-        <View style={{height:70}}>
-            <Text style={{textAlign:'center'}}>Julho</Text>
-        </View>
-
-        <View style={styles.Card}>
-            <Card elevation={5}>
-                <Card.Title 
-                    title="StarBucks" 
-                    subtitle="12 de Julho - 10:32" 
-                    right={(props) => <Text>10.00</Text>}
-                    left={(props) => <Avatar.Icon {...props} icon="mood" />}
+        <View style={{maxHeight:70, backgroundColor: '#8ee05a', paddingTop: 10, flex: 1, flexDirection: 'column', alignItems:'center', justifyContent: 'center'}}>
+            <View>
+                <Text style={{textAlign:'center', fontSize: 25, fontFamily: 'monospace'}}>Julho</Text>
+            
+                <ProgressBarAnimated
+                  width={200}
+                  borderColor={'#000000'}
+                  value={40}
+                  onComplete={() => {
+                    Alert.alert('Hey!', 'onComplete event fired!');
+                  }}     
                 />
-            </Card>
+            </View>
+        </View>
+        
+        <View style={{paddingTop: 15 }}>
+            <View style={styles.Card}>
+                <Card elevation={5}>
+                    <Card.Title 
+                        title="StarBucks" 
+                        fontFamily="monospace"
+                        subtitle="12 de Julho - 10:32" 
+                        right={(props) => <Text style={{padding: 20, fontSize: 25, fontFamily: 'monospace', color: '#f24730'}}>-10.00</Text>}
+                        left={(props) => <Avatar.Icon {...props} icon="mood" />}
+                    />
+                </Card>
+            </View>
+
+            <View style={styles.Card}>
+                <Card elevation={5}>
+                    <Card.Title 
+                        title="Papelaria" 
+                        subtitle="11 de Julho - 14:55" 
+                        right={(props) => <Text style={{padding: 20, fontSize: 25, fontFamily: 'monospace', color: '#f24730'}}>-12.50</Text>}
+                        left={(props) => <Avatar.Icon {...props} icon="book" />}   
+                    />
+                </Card>
+            </View>
         </View>
 
-        <View style={styles.Card}>
-            <Card elevation={5}>
-                <Card.Title 
-                    title="Papelaria" 
-                    subtitle="11 de Julho - 14:55" 
-                    right={(props) => <Text>10.00</Text>}
-                    left={(props) => <Avatar.Icon {...props} icon="book" />}   
-                />
-            </Card>
+        <View style={{width: '100%', flex:1, flexDirection:'row', height:70, backgroundColor: '#8ee05a', paddingTop: 10, position:'absolute',bottom:0}}>
+            <Text style={{flex:1, textAlign:'center', fontSize: 25, fontFamily: 'monospace', paddingTop:13}}>Saldo: -22.50</Text>     
         </View>
-
     </View>
   );
 }
@@ -56,7 +72,7 @@ HomeScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   container: {
-   // flex: 1,
+    flex: 1,
     marginTop: 30,
     backgroundColor: '#fff',
   },
